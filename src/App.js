415bfb0cc3170055f759259de54f97ba82dbe87f
nvillalon1994 <70975528+nvillalon1994@users.noreply.github.com>
2021-12-13 00:28:50 -0300

    cambio en h1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>this is my app</h1>
+        <h1>this is my app viste</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
